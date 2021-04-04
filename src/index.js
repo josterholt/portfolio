cbf1c6723b3f47a27c2b170e58c1ba4a9968718c
Justin Osterholt <shroder@gmail.com>
2021-04-04 06:33:06 -0700

    Cleaning up styling, adding missing bootstrap rules styling back in.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,12 @@ import {createOvermind} from 'overmind'
 import {Provider} from 'overmind-react'
 import {config} from './overmind'
 import './assets/tailwind.css'
+import './assets/app.css'
+import ItemList from './modules/Portfolio/components/ItemList'
 
 const overmind = createOvermind(config, {
     devtools: '127.0.0.1:3031',
 })
-
-import ItemList from './modules/Portfolio/components/ItemList'
-// import Http from 'cerebral-module-http';
-
-// const controller = Controller(Model({}));
-
-// controller.addModules({
-//   example: Portfolio(),
-//   http: Http({
-//   	baseUrl: '/data'
-//   })
-// });
 
 ReactDOM.render(
     <Provider value={overmind}>
